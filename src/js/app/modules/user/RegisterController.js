11872c5280d4d853d144a0e1f74fe50b4081a38f
elger <elgervb@gmail.com>
2015-03-13 23:22:13 +0100

    The user can sign up for a new account #4
    Sign up GUI

--- src/js/app/modules/user/RegisterController.js
+++ src/js/app/modules/user/RegisterController.js
@@ -2,8 +2,19 @@
  * Registers a new user. After signing up, the user gets an activation code per mail. With this 
  * code, the user can activate his account.
  */
-app.controller('RegisterController', function($scope){
+app.controller('RegisterController', function($scope, $location){
 
+  /**
+   * Cancels the registration and takes the user back to the main page
+   */
+	$scope.cancel = function(){
+    $location.path('/');
+  };
 
+	$scope.register = function(){
+
+		$scope.registered = !$scope.registered;
+
+	};
 
 });
