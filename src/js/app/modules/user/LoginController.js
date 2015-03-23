bbcac33d75aedc1c4413a63b13f62574ce346a67
Wouter <tuvokki@gmail.com>
2015-03-23 21:31:30 +0100

    Login logic

--- src/js/app/modules/user/LoginController.js
+++ src/js/app/modules/user/LoginController.js
@@ -2,13 +2,21 @@
  * Sign in an existing user with userID and password. Only active accounts can log in. 
  * When user signs in, he will be redirected to the dashboard.
  */
-app.controller('LoginController', function($scope){
+app.controller('LoginController', function($scope, $http){
 
 	/**
 	 * Tries to login the user, using the login form's fields
 	 */
 	$scope.login = function(){
-
+    $http.post('/api/login', {msg:'hello word!'}).
+      success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
 	};
 
 });
