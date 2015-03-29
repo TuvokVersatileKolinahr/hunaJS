e07e8e71a290abc2fda0664341687fcc65718059
elger <elgervb@gmail.com>
2015-03-29 21:31:33 +0200

    The user can login #3
    fixed #3

--- src/js/app/modules/user/LoginController.js
+++ src/js/app/modules/user/LoginController.js
@@ -2,21 +2,30 @@
  * Sign in an existing user with userID and password. Only active accounts can log in. 
  * When user signs in, he will be redirected to the dashboard.
  */
-app.controller('LoginController', function($scope, $http){
+app.controller('LoginController', function($scope, $http, $location){
 
 	/**
 	 * Tries to login the user, using the login form's fields
 	 */
 	$scope.login = function(){
     $http.post('/api/login', {
-        username: $scope.form.username.$modelValue,
-        password: $scope.form.password.$modelValue 
+        username: $scope.username,
+        password: $scope.password 
       })
       .success(function(data, status, headers, config) {
-        console.log("data, status, headers, config", data, status, headers, config);
+        
+        // Login succeeded, commence to dashboard
+        $location.url("/dashboard");
       })
       .error(function(data, status, headers, config) {
         console.log("data, status, headers, config", data, status, headers, config);
+        
+        // show an error
+        $scope.error = true;
+
+        // reset form
+        $scope.login = {};
+        $scope.form.$setPristine(); 
     });
 	};
 
