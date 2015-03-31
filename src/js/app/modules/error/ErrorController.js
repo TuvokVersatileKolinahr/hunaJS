4aeae89ec84c8c99a182cb2e54795f9b93fb155f
elger <elgervb@gmail.com>
2015-03-31 23:06:32 +0200

    create erroneous demo page #8
    fixed #8

--- src/js/app/modules/error/ErrorController.js
+++ src/js/app/modules/error/ErrorController.js
@@ -1,19 +1,51 @@
 /**
  * Main controller for the Huna JS app. Shows the main (index) page
  */
-app.controller('ErrorController', function($scope, $location){
+app.controller('ErrorController', function($scope, $interval){
+
+  var stop;
+  $scope.timeout = "";
+  $scope.busy = false;
 
   $scope.parseError = function(){
+    countDown();
     var a = "";
     a.asdf = asdf;
   };
 
   $scope.syntaxError = function(){
+    countDown();
     eval("var a = b;");
   };
 
   $scope.uncaughtException = function(){
+    countDown();
     throw new Error("Uncaught Exception...");
   };
+
+  $scope.stop = function(){
+    if (angular.isDefined(stop)){
+      $interval.cancel(stop);
+      stop = undefined;
+      $scope.timeout = "";
+      $scope.busy = false;
+    }
+  };
+
+  function countDown(){
+    $scope.busy = true;
+    $scope.timeout = 8;
+    stop = $interval(function(){
+      $scope.timeout--;
+      if ($scope.timeout <= 0){
+        $scope.stop();
+      }
+    }, 1000);
+  }
+
+
+  $scope.$on('$destroy', function() {
+    $scope.stop();
+  });
 
 });
