ab5697fdef57216864de553d268c995a54c82ccc
elger <elgervb@gmail.com>
2015-04-06 23:44:40 +0200

    make it a spline chart...
    Add dashboard #10

--- src/js/app/modules/dashboard/DashboardController.js
+++ src/js/app/modules/dashboard/DashboardController.js
@@ -17,11 +17,12 @@ app.controller('DashboardController', function($scope, DashboardServices){
         ['warnings', 30, 200, 100, 300, 150, 250],
         ['errors'  , 50,  20,  10,  40,  15,  25],
         ['info'    , 80,  75, 157,  86, 136, 182]
-      ]
+      ],
+      type: 'spline'
     },
     axis: {
       x: {
-        type: 'timeseries',
+        type: 'categories',
         categories: ['1 March 2015','2 March 2015','3 March 2015','4 March 2015','5 March 2015','6 March 2015']
       },
       y: {
