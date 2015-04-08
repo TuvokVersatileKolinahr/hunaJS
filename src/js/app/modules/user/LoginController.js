54282eb575be4273be5b9396a148452d06ee2168
Wouter <tuvokki@gmail.com>
2015-04-08 10:37:54 +0200

    Fixed login

--- src/js/app/modules/user/LoginController.js
+++ src/js/app/modules/user/LoginController.js
@@ -8,16 +8,26 @@ app.controller('LoginController', function($scope, $http, $location){
 	 * Tries to login the user, using the login form's fields
 	 */
 	$scope.login = function(){
-    $http.post('/api/login', {
-        username: $scope.username,
-        password: $scope.password 
-      })
-      .success(function(data, status, headers, config) {
-        
+    $http({
+      method: 'POST',
+      url: '/api/login',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      transformRequest: function(obj) {
+          var str = [];
+          for(var p in obj)
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          return str.join("&");
+      },
+      data: {
+        username: $scope.form.username.$modelValue,
+        password: $scope.form.password.$modelValue
+      }
+    })
+    .success(function(data, status, headers, config) {
         // Login succeeded, commence to dashboard
         $location.url("/dashboard");
-      })
-      .error(function(data, status, headers, config) {
+    })
+    .error(function(data, status, headers, config) {
         console.log("data, status, headers, config", data, status, headers, config);
         
         // show an error
@@ -27,6 +37,5 @@ app.controller('LoginController', function($scope, $http, $location){
         $scope.login = {};
         $scope.form.$setPristine(); 
     });
-	};
-
+  };
 });
