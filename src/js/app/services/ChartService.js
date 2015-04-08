63c34c42fb19df42e2ac497207f6dd4f10f5b049
elger <elgervb@gmail.com>
2015-04-08 14:45:07 +0200

    fixed typo to load correct mock data

--- src/js/app/services/ChartService.js
+++ src/js/app/services/ChartService.js
@@ -14,7 +14,7 @@ app.service('ChartService', function($q){
         ['errors'  , 50,  50,  10,  60,  15,  25],
         ['info'    , 80,  50, 157,  77, 26, 88]
       ],
-      'tovok.nl': [
+      'tuvok.nl': [
         ['warnings', 60, 120, 30, 150, 280, 60],
         ['errors'  , 20,  50,  40,  10,  6,  20],
         ['info'    , 90,  56, 78,  120, 80, 99]
