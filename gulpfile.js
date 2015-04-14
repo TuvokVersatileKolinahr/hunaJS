c1658aba9820ba1aa60e62f3e37ed044ba1dddbd
elger <elgervb@gmail.com>
2015-04-14 23:14:15 +0200

    fixed c3 css

--- gulpfile.js
+++ gulpfile.js
@@ -253,7 +253,6 @@ gulp.task('styles', function() {
       minifycss = require('gulp-minify-css'),
       sass = require('gulp-sass');
 
-  return gulp.src('src/styles/main.scss')
     .pipe(plumber(options.plumberConfig()))
     .pipe(sass({ style: 'expanded' }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
