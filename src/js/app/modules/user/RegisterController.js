8361571ba8fd22737d1270f583d258469aa2c4dc
elger <elgervb@gmail.com>
2015-04-28 21:48:20 +0200

    Register does not have error handling #32
    fixed #32

--- src/js/app/modules/user/RegisterController.js
+++ src/js/app/modules/user/RegisterController.js
@@ -19,12 +19,24 @@ app.controller('RegisterController', function($scope, $location, $http){
         email: $scope.form.email.$modelValue,
         password: $scope.form.password.$modelValue
       }
-    }).success(function(data, status, headers, config) {
-        console.log("data, status, headers, config", data, status, headers, config);
-        $scope.registered = !$scope.registered;
-      }).
-      error(function(data, status, headers, config) {
-        console.log("data, status, headers, config", data, status, headers, config);
+    })
+    .success(function(data, status, headers, config) {
+      $scope.registered = !$scope.registered;
+    })
+    .error(function(data, status, headers, config) {
+      // #32 error handling
+      if (status === 409){
+        $scope.error = "User already exists. Please pick another username.";
+
+        // empty field and set focus
+        delete $scope.user.username;
+        document.getElementById('username').focus();
+        document.getElementById('username').scrollIntoView();
+      }
+      else{
+        $scope.error = "Registering the user " + data.username + " failed. Please try again later." ;
+        console.error($scope.error);
+      }
     });
 
 	};
