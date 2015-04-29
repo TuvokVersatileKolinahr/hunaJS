30052d602be231b0fa41808c40b8ea6e91d7410f
Wouter <tuvokki@gmail.com>
2015-04-29 14:48:16 +0200

    Add cache to make user survive page reload fixes #30

--- src/js/app/services/AuthService.js
+++ src/js/app/services/AuthService.js
@@ -1,21 +1,56 @@
-app.factory("AuthService", function($rootScope) {
+app.factory("AuthService", function($rootScope, CacheFactory) {
   var isAuthenticated = function() {
     console.log("check you");
+
+    // Lazy load the cache
+    if (!CacheFactory.get('userCache')) {
+      userCache = CacheFactory.createCache('userCache', {
+        // Cache will hold data in localStorage if available.
+        // Data is not cleared when the page is refreshed
+        storageMode: 'localStorage' 
+      });
+    }
+
     if ($rootScope.auth && $rootScope.auth.user) {
       return true;
     } else {
-      return false;
+      //check the cache for a user
+      var userCache = CacheFactory.get('userCache');
+      if (userCache) {
+        var user = userCache.get('auth');
+        if (user) {
+          $rootScope.auth = user;
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   },
+
   saveLogin = function(user) {
     console.log("Saving ... ", user);
+
+    // Lazy load the cache
+    if (!CacheFactory.get('userCache')) {
+      userCache = CacheFactory.createCache('userCache', {
+        // Cache will hold data in localStorage if available.
+        // Data is not cleared when the page is refreshed
+        storageMode: 'localStorage' 
+      });
+    }
+
+    var userCache = CacheFactory.get('userCache');
+    userCache.put('auth', user);
     $rootScope.auth = user;
     return true;
   },
+
   clearLogin = function() {
     console.log("User in scope: ", $rootScope.auth.user);
     delete $rootScope.auth.user;
   },
+
   getToken = function() {
     return $rootScope.auth.user.token;
   };
